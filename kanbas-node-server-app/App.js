c9009c761244866b4b802c45b534abe4d2db286d
Novel <50412055+20bhayward@users.noreply.github.com>
2024-03-30 13:29:22 -0400

    Current Progress

---
+++ kanbas-node-server-app/App.js
@@ -0,0 +1,9 @@
+import express from "express";
+import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import cors from "cors";
+const app = express();
+app.use(cors());
+CourseRoutes(app);
+Lab5(app);
+app.listen(4000);
